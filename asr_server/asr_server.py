875f72d858eceeef2c977253ee09049ff70659af
jfajkowski <szczekalem213>
2017-12-14 21:58:29 +0100

    Cleaning corpora improved. New scrapers.

--- asr_server/asr_server.py
+++ asr_server/asr_server.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def decode():
-    return 'Decode!'
+@app.route('/upload')
+def load_page():
+    return render_template('upload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(file.filename)
+        return 'file uploaded successfully'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
